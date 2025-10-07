d33ddf34fe3bc15cf48e4c409e88ac4503cc9e8e
Lance <lancehemphill2000@gmail.com>
2025-10-06 22:55:49 -0400

    install react, started homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import react from "react";
+
+const Home = () => {
+  
+  return (
+    <div className="bg-base">
+    <main className="max-w-7xl mx-auto px-4 py-24 h-screen">
+      <section className="space-y-4">
+        <div className="flex flex-col items-center gap-2 primary-text">
+          <h1 className="text-5xl font-bold w-fit">Lance Hemphill</h1>
+          <p className="text-2xl font-light w-fit">Software Engineer</p>
+        </div>
+
+        
+        <div className="h-12 rounded-lg surface-2">
+          
+        </div>
+      </section>
+    </main>
+    </div>
+  )
+}
+
+
+export default Home;
