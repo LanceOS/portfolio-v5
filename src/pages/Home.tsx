64bff095a1392dc4d8ea6288e6de4bcc87657887
Lance <lancehemphill2000@gmail.com>
2025-10-10 14:13:54 -0400

    finished adding projects and links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ const Home = () => {
 
       <section className="space-y-2">
         <h2 className="text-3xl font-bold">About Me</h2>
-        <p className="text-xl font-light">Blah Blah Blah... Life story... I like software....</p>
+        <p className="text-xl font-light">I like software, I write software.</p>
       </section>
 
       <section className="space-y-4">
@@ -46,10 +46,39 @@ const Home = () => {
       </section>
 
 
-      <section className="space-y-4">
+      <section className="space-y-12">
         <h2 className="text-3xl font-bold">Projects</h2>
-        <div>
+        <div className="space-y-4">
           <h3 className="text-xl font-semibold">Svelte-MOBAS</h3>
+          <p className="font-light">{"(SvelteKit, MinIO, Drizzle, Docker, Docker-Compose, Better-Auth Authentication, Postgres, Git)"}</p>
+          <p className="font-light">A full stack application boilerplate with in depth code comments and instructions. This application is meant to be used as a boilerplate
+          or for junior devs who want to learn more about setting up a full stack application. Using services like Docker to host databases and libraries like Better-Auth to handle authentication
+          this repo is a good starting point for a full stack application.</p>
+          <a href="https://github.com/LanceOS/Svelte-MOBAS" target="_blank" aria-label="View project on github" className="py-2 px-4 bg-black text-white font-semibold text-sm cursor-pointer hover:px-6 duration-200">Github Repo</a>
+        </div>
+        <div className="space-y-4">
+          <h3 className="text-xl font-semibold">Simple Task App</h3>
+          <p className="font-light">{"(SvelteKit, Drizzle, Better-Auth, Postgres, Docker, Docker-Compose, RabbitMQ, Git)"}</p>
+          <p className="font-light">Like a true developer this project is incomplete, however it is maybe 75% complete. It is a full stack application that allows users to create groups/orgs and within those create task groups. These task groups house created tasks that can be assigned to memebers of the main group. This uses SvelteKit for the frontend and backend with SSR. Docker hosts the services like RabbitMQ and Postgres while drizzle manages the read and writing of data to the database as well as it's structure. Better-Auth is used for authentication.</p>
+          <a href="https://github.com/LanceOS/simple-task-app" target="_blank" aria-label="View project on github" className="py-2 px-4 bg-black text-white font-semibold text-sm cursor-pointer hover:px-6 duration-200">Github Repo</a>
+        </div>
+
+        <div className="space-y-4">
+          <h3 className="text-xl font-semibold">Link Sharing App</h3>
+          <p className="font-light">{"(NextJS, MongoDB, NextAuth, Git)"}</p>
+          <p className="font-light">This project is from the service Frontend Mentor. It is a full stack application that allows users to create a preview of links and then share them to anyone. The links are displayed on a smartphone along with the user's info and profile image. Using NextJS for SSR and NextAuth for authentication. User's can sign up and save their links which is saved in MongoDB. This is a much older project of mine and has not had any bug fixes since launch. However the app is still up and usable!</p>
+          <div className="flex gap-4">
+            <a className="py-2 px-4 bg-black text-white font-semibold text-sm cursor-pointer hover:px-6 duration-200" href="https://link-sharing-app-yzuc.vercel.app/auth/signin" target="_blank" aria-label="View project demo">Demo</a>
+            <a className="py-2 px-4 bg-black text-white font-semibold text-sm cursor-pointer hover:px-6 duration-200" href="https://github.com/LanceOS/link-sharing-app" target="_blank" aria-label="View project on github">Github Repo</a>
+          </div>
+        </div>
+      </section> 
+
+      <section className="space-y-4">
+        <h2 className="text-3xl font-bold">Links</h2>
+        <div className="flex gap-4">
+          <a href="https://www.linkedin.com/in/lancehemphill/" target="_blank" aria-label="My Linkedin" className="py-2 px-4 font-semibold bg-black text-white text-sm cursor-pointer hover:px-6 duration-200">LinkedIn</a>
+          <a href="https://github.com/LanceOS" target="_blank" aria-label="My Github" className="py-2 px-4 font-semibold bg-black text-white text-sm cursor-pointer hover:px-6 duration-200">Github</a>
         </div>
       </section>
     </main>
